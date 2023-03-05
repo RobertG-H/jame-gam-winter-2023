bf9d762a0e248a89ab6b7079dbb8b8bf9b0da440
reidaaron <38885567+reidaaron@users.noreply.github.com>
2023-03-05 10:49:18 -0500

    Add multiply events

--- jame-gam-winter-2023/Assets/Character/MultiplyHandler.cs
+++ jame-gam-winter-2023/Assets/Character/MultiplyHandler.cs
@@ -17,7 +17,7 @@ public class MultiplyHandler : MonoBehaviour
 
     Multiplizer multiplizer;
     Camera mainCamera;
-
+    ITriggerOnMultiply multiplyEvent;
     bool selected = false;
 
     private void Awake ()
@@ -75,6 +75,11 @@ public class MultiplyHandler : MonoBehaviour
         // apply ghostblue
         ghostRenderer = TryToFindMeshRenderer (ghost);
         ghostRenderer.material = targetingMaterial;
+
+        if (TryGetComponent<ITriggerOnMultiply>(out ITriggerOnMultiply multiplyEvent))
+        {
+            multiplyEvent.MultiplyEvent();
+        }
     }
 
     private void InitializeTargetPlane ()
