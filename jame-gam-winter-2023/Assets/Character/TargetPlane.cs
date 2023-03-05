4a028c1a13a3cde1af2bf1eb58c7ed1b8c63ebf6
Adam Sigal <adamsigal@gmail.com>
2023-03-05 12:54:38 -0500

    multiplizer circle limiting

--- jame-gam-winter-2023/Assets/Character/TargetPlane.cs
+++ jame-gam-winter-2023/Assets/Character/TargetPlane.cs
@@ -4,53 +4,44 @@ using UnityEngine;
 
 public class TargetPlane : MonoBehaviour
 {
-    Vector3 cachedHitPoint = Vector3.zero;
+    Vector3 cachedHitPoint;
     int layerMask;
     void Awake()
     {
         layerMask = LayerMask.GetMask("Targetable");
     }
-    public Vector3 RaycastToEdge(Vector3 cameraPosition, Vector3 cameraDirection)
+    public Vector3 RaycastToEdge(Vector3 cameraPosition, Vector3 cameraDirection, GameObject origObj)
     {
+        this.cachedHitPoint = origObj.transform.position;
+        
         RaycastHit hit;
-        Debug.DrawRay (cameraPosition, cameraDirection * 100f, Color.black, 5);
-        if (Physics.Raycast(cameraPosition, cameraDirection, out hit, Mathf.Infinity, layerMask)){
-            //print("Found an object - distance: " + hit.distance);
-            //print("Found an object: " + hit.collider.gameObject);
-            GameObject targetObj = hit.collider.gameObject;
-
+        //Debug.DrawRay (cameraPosition, cameraDirection * 100f, Color.black, 5);
+        // ray 
+        if (Physics.Raycast(cameraPosition, cameraDirection, out hit, Mathf.Infinity, layerMask))
+        {
             Vector3 vecHit2Obj = transform.position - hit.point;
             cachedHitPoint = hit.point;
-            return hit.point;
             
-            // if (targetObj.tag == "TargetPlaneTag")
-            // {
-                //Debug.DrawRay (hit.point, vecHit2Obj * 100f, Color.cyan, 5f);
-                //Debug.Log ($"Attempting edge hit: {hit.point} and{vecHit2Obj}");
-                // int layerMask = LayerMask.GetMask ("TargetPlaneTargetable");
+            // can go on circle of radius 5
+            float maxObjDim = 5f;
 
-                // RaycastHit edgeHit;
-                //  if (Physics.Raycast(hit.point, vecHit2Obj, out edgeHit, Mathf.Infinity, layerMask))
-                //  {
-                //     //print ("Found an object - distance: " + edgeHit.distance);
-                //     //print ("Found an object: " + edgeHit.collider.gameObject);
-                //     cachedHitPoint = edgeHit.point;
+            Vector3 vecObj2hit = hit.point - origObj.transform.position;
 
-                //     Vector3 manualOffset = vecHit2Obj.normalized * 0.2f;
-                //     return edgeHit.point - manualOffset;
-                //  }
-            // }//TODO: Make this more efficient
-            // else if (targetObj.TryGetComponent<MultiplyHandler> (out MultiplyHandler handler))
-            // {
-            //     cachedHitPoint = hit.point;
-            //     return hit.point;
-            // }
-            // else if(targetObj.GetComponentInParent<MultiplyHandler>())
-            // {
-            //     cachedHitPoint = hit.point;
-            //     return hit.point;
-            // }
+            // ray hit is beyond max circle around origObj
+            if (maxObjDim < vecObj2hit.magnitude)
+            {
+                return origObj.transform.position + (maxObjDim*vecObj2hit.normalized);
+            }
+            // ray hit is between max circle perimiter and origObj
+            else
+            {
+                return hit.point;
+            }
         }
-        return cachedHitPoint;
+        // if you hit origObj
+        else
+        {
+            return cachedHitPoint;
+        }
     }
 }
